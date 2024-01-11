768583a517f70f2decef5bfac6a0f34b08b6b9d1
AndreaGul <andreagulino99@gmail.com>
2024-01-11 01:26:19 +0100

    feat: form e chiamata alla pagina stessa

--- index.php
+++ index.php
@@ -11,7 +11,24 @@
 </head>
 
 <body>
-
+  <div  class="container">
+    <h1>
+      Iscrizione Newsletter
+    </h1>
+  <form action="index.php" method="GET">
+      
+      <div class=" mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" id="email"  name="email" placeholder="name@example.com">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+      </div>
+  </form>
+  </div>
+    <?php
+  echo $email = $_GET['email']
+  ?>
 </body>
 
 </html>
